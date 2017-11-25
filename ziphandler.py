a4b6e23a058cd96e3a2288850c7b23120e55d76c
cur065 <cur065@gmail.com>
2017-11-25 18:59:40 -0500

    End of Boto3 lesson

--- ziphandler.py
+++ ziphandler.py
@@ -1,10 +1,19 @@
 """ Find and read zip file """
 
+import StringIO
+import zipfile
 import boto3
+import mimetypes
 
 s = boto3.resource("s3")
 mb = s.Bucket("5934-build")
 
-for obj in mb.objects.all():
-    print obj.key
-    mb.download_file(obj.key, obj.key)
+mySite_zip = StringIO.StringIO()
+mb.download_fileobj("MySite.zip", mySite_zip)
+
+with zipfile.ZipFile(mySite_zip) as myzip:
+    for nm in myzip.namelist():
+        nmObj = myzip.open(nm)
+        # Do we really need ExtraArgs & mimetype?
+        mb.upload_fileobj(nmObj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        # mb.Object(nm).Acl().put(ACL='public-read')
